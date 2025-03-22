03d88e2ed11f298a295e33ffe0752076e1821dd3
munir <smsirajulmonir@gmail.com>
2025-03-22 15:31:13 +0600

    metadata added for seo

---
+++ src/app/blogs/create/page.tsx
@@ -0,0 +1,14 @@
+import BlogForm from '@/components/ui/BlogForm';
+import React from 'react';
+
+const CreateBlogPage = () => {
+
+    
+    return (
+        <div>
+            <BlogForm/>
+        </div>
+    );
+};
+
+export default CreateBlogPage;
